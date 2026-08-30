5febbf91cad2c2564332b0fbe9a9e69b9c3e6fa2
Balaji <mgbalaji89@gmail.com>
2026-08-30 18:57:53 +0530

    Create blackandscope.js

---
+++ blackandscope.js
@@ -0,0 +1,7 @@
+const city = "New York City";
+
+const logCitySkyline = () => {
+  let sckscrapper = 'Empire State Building';
+  return 'The Stars over the ' + sckscrapper + 'in' + city;
+};
+console.log(logCitySkyline());
